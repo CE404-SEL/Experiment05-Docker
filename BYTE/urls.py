286dc943dfbdc90e1ee25dfe9d5b6b0d3326156b
Moeein Aali <moeeeinaali@gmail.com>
2025-08-18 22:18:32 +0330

    Refactor urls.py

--- BYTE/urls.py
+++ BYTE/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from blog import views as blog_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('authors/', blog_views.AuthorListCreate.as_view(), name='author-list-create'),
-    path('authors/<int:pk>/', blog_views.AuthorDetail.as_view(), name='author-detail'),
-    path('authors/readall/', blog_views.AuthorReadAll.as_view(), name='author-readall'),
-
-    path('blogs/', blog_views.BlogListCreate.as_view(), name='blog-list-create'),
-    path('blogs/<int:pk>/', blog_views.BlogDetail.as_view(), name='blog-detail'),
-    path('blogs/readall/', blog_views.BlogReadAll.as_view(), name='blog-readall'),
+    path('api/', include('blog.urls'))
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from blog import views as blog_views
 
 urlpatterns = [
-    # Author URLs
+    path('authors/', blog_views.AuthorListCreate.as_view(), name='author-list-create'),
+    path('authors/<int:pk>/', blog_views.AuthorDetail.as_view(), name='author-detail'),
+    path('authors/readall/', blog_views.AuthorReadAll.as_view(), name='author-readall'),
 
+    path('blogs/', blog_views.BlogListCreate.as_view(), name='blog-list-create'),
+    path('blogs/<int:pk>/', blog_views.BlogDetail.as_view(), name='blog-detail'),
+    path('blogs/readall/', blog_views.BlogReadAll.as_view(), name='blog-readall'),
 ]
