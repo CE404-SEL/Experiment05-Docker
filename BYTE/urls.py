321f509b34a987e9db505c342b385e5bbbe3abbf
Moeein Aali <moeeeinaali@gmail.com>
2025-08-18 16:42:16 +0330

    Add blog admin and urls

--- BYTE/urls.py
+++ BYTE/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for BYTE project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('authors/', blog_views.AuthorListCreate.as_view(), name='author-list-create'),
+    path('authors/<int:pk>/', blog_views.AuthorDetail.as_view(), name='author-detail'),
+    path('authors/readall/', blog_views.AuthorReadAll.as_view(), name='author-readall'),
+
+    path('blogs/', blog_views.BlogListCreate.as_view(), name='blog-list-create'),
+    path('blogs/<int:pk>/', blog_views.BlogDetail.as_view(), name='blog-detail'),
+    path('blogs/readall/', blog_views.BlogReadAll.as_view(), name='blog-readall'),
 ]
